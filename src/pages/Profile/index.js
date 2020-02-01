db9dab36ac2ec49456f30d955b56fce8e4abd3b2
Jeferson GB <jefersongb@outlook.com.br>
2020-01-31 22:09:29 -0300

    Configura o profile para alteração de dados.

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,33 @@
 import React from "react";
-
-// import { Container } from './styles';
+import { useSelector } from "react-redux";
+import { Form, Input } from "@rocketseat/unform";
+import { Container } from "./styles";
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input type="password" name="oldPassword" placeholder="Nova senha" />
+        <Input type="password" name="password" placeholder="Seu senha atual" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
